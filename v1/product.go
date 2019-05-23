c5f87a6deb9d0caa2deb3db0e002cce281c86ef0
FadhHaw <fadhlan.hawali@tokopedia.com>
2019-05-23 22:50:47 +0700

    added delete product

--- v1/product.go
+++ v1/product.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"github.com/dgrijalva/jwt-go"
 	"fmt"
+	"github.com/gorilla/mux"
+	"strconv"
 )
 
 func (db *InDB) AddProduct (w http.ResponseWriter, r *http.Request){
@@ -78,6 +80,42 @@ func (db *InDB) ListProduct (w http.ResponseWriter, r *http.Request){
 	}
 
 	utils.WrapAPIData(w,r,product,http.StatusOK,"success")
+}
+
+func (db *InDB) DeleteProduct (w http.ResponseWriter, r *http.Request){
+	var id int
+	var id_product int
+	var err error
+	if r.Method != "DELETE"{
+		utils.WrapAPIError(w,r,http.StatusText(http.StatusMethodNotAllowed),http.StatusMethodNotAllowed)
+		return
+	}
+
+	id_product_temp := mux.Vars(r)["id"]
+	if id_product,err = strconv.Atoi(id_product_temp);err != nil{
+		utils.WrapAPIError(w,r,"error converting string to integer",http.StatusInternalServerError)
+		return
+	}
+	if token := r.Context().Value(TokenContextKey); token != nil {
+		tokenMap := token.(jwt.MapClaims)
+		tempId := tokenMap["id"].(float64)
+		id = int(tempId)
+	} else {
+		utils.WrapAPIError(w,r,"invalid token",http.StatusBadRequest)
+		return
+	}
+
+	tx := db.DB.MustBegin()
+	tx.Get(&id, fmt.Sprintf("SELECT id FROM STORES WHERE id_user = %d",id))
+	tx.MustExec(fmt.Sprintf("DELETE from products where id_store = %d AND id = %d",id,id_product))
+	//tx.MustExec("INSERT INTO products (prod_name,quantity,description,price,url_pic,id_store) VALUES (?, ? ,? , ?, ?, ?)",product.NamaBarang,product.Quantity,product.Deskripsi,product.Harga,product.UrlGambar,id)
+
+	if err := tx.Commit(); err != nil {
+		utils.WrapAPIError(w,r,"error getting product",http.StatusInternalServerError)
+		return
+	}
+
+	utils.WrapAPISuccess(w,r,"success deleting product",http.StatusOK)
 }
 
 type Product struct {
